c8fe57c19146626177973cc0f611ebf9c463b723
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2023-11-25 11:51:05 -0500

    return loan function completed

--- routes/loan.py
+++ routes/loan.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Response, status
 from fastapi.responses import JSONResponse
 from config.db import conn
+from models.book import books
 from models.loan import loans
 from schemas.loan import Loan, LoanPut, LoanReturn
 from starlette.status import HTTP_204_NO_CONTENT
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import func,select
+from sqlalchemy import func, select, text, update
 
 loan = APIRouter()
 
@@ -40,10 +41,7 @@ def get_loan(id: str):
 @loan.get("/loan", response_model=list[Loan], tags=["loans"])
 def get_loans():
     result = conn.execute(loans.select()).fetchall()
-    result_dicts = [{"id": row[0], "employee_id": row[1], "user_name": row[2], "book_id": row[3],
-                     "loan_date": row[4], "devolution_date": row[5], "return_date": row[6]} for row in result]
-    response = {"data": result_dicts}
-    return response
+    return result
 
 
 @loan.delete("/loan/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["loans"])
@@ -73,26 +71,65 @@ def return_loan(id: str, loan: Loan):
         return_date=loan.return_date
     ).where(loans.c.id == id))
     result_date = conn.execute(loans.select().where(loans.c.id == id)).first()
-    print("result date:" ,result_date)
-    difference = result_date[6] - result_date[5]
-
+    print("result date:", result_date)
 
     book_id = conn.execute(loans.select().where(loans.c.id == id)).first()[3]
-    from sqlalchemy import text
 
-    consulta = text("SELECT COUNT(*) FROM loans WHERE book_id = book_id")
+    consulta = text(f"SELECT COUNT(*) FROM loans WHERE book_id = {book_id}")
     count = conn.execute(consulta).scalar()
-
-
-
-    
-    #consulta = select([func.count()]).where(loans.c.book_id == "1")
-    #resultado = conn.execute(consulta).scalar()
-
     print("Count:", count)
+
+    loans_with_same_book_id = conn.execute(
+        select(loans).where(loans.c.book_id == book_id)).fetchall()
+
+    print("loans_with_same_book_id:", loans_with_same_book_id)
+
+    total_difference = 0
+    for loan_row in loans_with_same_book_id:
+        total_difference += (loan_row.return_date -
+                             loan_row.devolution_date).days
+
+    count = len(loans_with_same_book_id)
+    average_difference = total_difference / count
+    suggestion = round(average_difference, 2)
+    print("Suggestion:", suggestion)
+
+    update_statement = update(books).values(Suggestion=suggestion).where(
+        books.c.book_id == book_id)
+    conn.execute(update_statement)
+
+    difference = result_date[6] - result_date[5]
 
     print("Difference:", difference)
     if not result.rowcount:
         return JSONResponse(status_code=404, content={"detail": "Loan not found"})
     conn.commit()
     return conn.execute(loans.select().where(loans.c.id == id)).first()
+
+
+@loan.post("/loan/update_suggestions", response_model=None, tags=["loans"])
+def update_suggestions():
+    loans_list = conn.execute(loans.select()).fetchall()
+    for loan in loans_list:
+        book_id = loan[3]
+        consulta = text(
+            f"SELECT COUNT(*) FROM loans WHERE book_id = {book_id}")
+        count = conn.execute(consulta).scalar()
+
+        loans_with_same_book_id = conn.execute(
+            select(loans).where(loans.c.book_id == book_id)).fetchall()
+
+        total_difference = 0
+        for loan_row in loans_with_same_book_id:
+            total_difference += (loan_row.return_date -
+                                 loan_row.devolution_date).days
+
+        count = len(loans_with_same_book_id)
+        average_difference = total_difference / count
+        suggestion = round(average_difference, 2)
+
+        update_statement = update(books).values(Suggestion=suggestion).where(
+            books.c.book_id == book_id)
+        conn.execute(update_statement)
+
+    conn.commit()

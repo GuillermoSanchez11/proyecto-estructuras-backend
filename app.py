1753e1d61a687ecf64e9b6306253380903be3601
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2023-11-27 18:41:07 -0500

    config middleware

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from routes.loan import loan
 from routes.employee import employee
 from routes.book import book
 from routes.employees_per_day import employee_per_day
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
@@ -12,3 +13,16 @@ app.include_router(loan)
 app.include_router(employee)
 app.include_router(book)
 app.include_router(employee_per_day)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",  # Agrega aquí la URL de tu aplicación cliente
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)

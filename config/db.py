74af3ab9536b1580c18b93b0b8efb09228a2de28
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2023-11-30 09:32:25 -0500

    librarytest.sql added

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, MetaData
 
 
 engine = create_engine(
-    "mysql+pymysql://root:root_password@localhost:3306/librarydb")  # librarybstest
+    "mysql+pymysql://root:root_password@localhost:3306/librarydbtest")  # librarydbtest
 
 conn = "engine.connect()"
 meta = MetaData()
